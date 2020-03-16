12a20e519291f0877a3b63dd88525a49d46e7d61
zzy <906922090@qq.com>
2020-03-16 23:12:45 +0800

    首页布局

--- app/src/main/java/com/example/application1/base/BaseActivity.java
+++ app/src/main/java/com/example/application1/base/BaseActivity.java
@@ -1,5 +1,13 @@
 package com.example.application1.base;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -7,5 +15,6 @@ import androidx.fragment.app.Fragment;
  * @date: 2020-03-16
  */
 public class BaseActivity extends Fragment {
+
 
 }
